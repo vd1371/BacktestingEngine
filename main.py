e51ab88af1537ebf4c6b61b36a2bfc686f4ed37f
Vahid Asghari <vd1371@gmail.com>
2024-03-21 16:43:10 +0800

    update: activated the simulate

--- main.py
+++ main.py
@@ -43,9 +43,9 @@ if __name__ == "__main__":
     ## GUIDE: Step 1
 
     # run(run_mode="analyze_raw_data")
-    # run(run_mode="simulate")
+    run(run_mode="simulate")
     # run(run_mode="research")
-    run(run_mode="optimize")
+    # run(run_mode="optimize")
 
     profile.disable()
     profile.dump_stats(os.path.join("reports", "profile.prof"))
